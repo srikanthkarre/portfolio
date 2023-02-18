69635415d3e92c737c5d3ba26af0782e3f055fd1
srikanthkarre <ksrikanth910@gmail.com>
2023-02-18 15:23:17 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -71,7 +71,7 @@ $(function() {
 
   sr.reveal('.background');
   sr.reveal('.skills');
-  sr.reveal('.experience', { viewFactor: 0.2 });
+  sr.reveal('.experience', { viewFactor: 0.05 });
   sr.reveal('.featured-projects', { viewFactor: 0.1 });
   sr.reveal('.other-projects', { viewFactor: 0.05 });
 });
